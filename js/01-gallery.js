a2ae96fd3102d68f814b66d8fce1751443a37df5
JacekMielanczuk <mielanczuk1988@gmail.com>
2023-02-12 12:32:10 +0100

    zadanie 1 - galeria
    zadanie 1 - galeria

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original.value}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+    })
+    .join("");
+}
